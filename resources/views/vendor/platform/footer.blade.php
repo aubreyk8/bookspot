f126d50594c6ae0a2898c05eeec83c1403f92532
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-04-29 22:38:08 +0200

    [DEV-77] Updates footer

--- resources/views/vendor/platform/footer.blade.php
+++ resources/views/vendor/platform/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="text-center">
     <p class="small m-n">
-      {{ __('The application code is published under the MIT license.') }} 2016 - {{date('Y')}}<br>
+      {{ __('BookSpot') }} - &copy; Copyright {{date('Y')}}<br>
         <a href="http://orchid.software" target="_blank" rel="noopener">
-            {{ __('Currently') }} v{{\Orchid\Platform\Dashboard::VERSION}}
+            v {{ENV('APP_VERSION')}}
         </a>
     </p>
 </div>
