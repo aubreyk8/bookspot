67edf61b60089b2302f0e71397e7b7550e8b4c04
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-05 21:49:30 +0200

    [DEV-83] Added public controller and exceptions

---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Services\PublishingManager;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Contracts\View\Factory;
+use App\Exceptions\PublicationNotFoundException;
+
+/**
+ * Class PublicController
+ * @package App\Http\Controllers
+ */
+class PublicController extends Controller
+{
+    /**
+     * @var PublishingManager
+     */
+    private PublishingManager $publicationManager;
+
+    /**
+     * PublicController constructor.
+     * @param PublishingManager $publicationManager
+     */
+    public function __construct(PublishingManager $publicationManager)
+    {
+        $this->publicationManager = $publicationManager;
+    }
+
+    /**
+     * @return RedirectResponse
+     */
+    public function index(): RedirectResponse
+    {
+        return redirect()->route('platform.main');
+    }
+
+    /**
+     * @param string $slug
+     * @return Factory|View
+     * @throws PublicationNotFoundException
+     */
+    public function publication(string $slug): View
+    {
+        $book = $this->publicationManager->fromSlug($slug);
+        return view('theme.fisher.index', compact('book'));
+    }
+}
