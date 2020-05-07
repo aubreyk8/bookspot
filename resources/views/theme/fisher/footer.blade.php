3a215bef53fa94f2ee2b90041fe182ef3c3029db
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-07 21:38:08 +0200

    [DEV-80] Added footer

--- resources/views/theme/fisher/footer.blade.php
+++ resources/views/theme/fisher/footer.blade.php
@@ -2,42 +2,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <nav class="navbar navbar-default" role="navigation">
-                    <div class="container-fluid">
-                        <!-- Brand and toggle get grouped for better mobile display -->
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="#">
-                                <span class="fa fa-book"></span> BookSpot
-                            </a>
-                        </div>
-
-                        <!-- Collect the nav links, forms, and other content for toggling -->
-                        <div class="collapse navbar-collapse text-center" id="bs-example-navbar-collapse-1">
-                            <div class="social-link center">
-                                <ul>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-facebook"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa fa-twitter"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-
-                        </div><!-- /.navbar-collapse -->
-
-                    </div><!-- /.container-fluid -->
-                </nav>
+                <div style="display: block;font-weight: bold" class="text-center">
+                    <span class="fa fa-book"></span> BookSpot &copy; All Rights Reserved.
+                </div>
             </div>
         </div>
     </div>
