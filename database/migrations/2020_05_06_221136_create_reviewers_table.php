4293479c65f8d32ef27543bf5fd228eb8fa5e0bd
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-07 21:16:03 +0200

    [DEV-80] Added migrations and seeds

---
+++ database/migrations/2020_05_06_221136_create_reviewers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviewers', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('names');
+            $table->string('job_title');
+            $table->string('facebook')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('message');
+            $table->bigInteger('book_id', false, true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviewers');
+    }
+}
