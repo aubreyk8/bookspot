f60e334a841ff57223af4697da6b84d52b8fbef6
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-07 21:41:03 +0200

    [DEV-80] Update reviewers section

--- resources/views/theme/fisher/reviewers.blade.php
+++ resources/views/theme/fisher/reviewers.blade.php
@@ -1,55 +1,67 @@
-<section id="team">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="title wow pulse" data-wow-delay=".5s">
-                    <h2>Our Happy Teams</h2>
-                    <p>
-                        Here is the people behind this great app
-                    </p>
+@if(!empty($book->reviewer))
+    <section id="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="title wow pulse" data-wow-delay=".5s">
+                        <h2>Reviewers</h2>
+                        <p>
+                            The following reviewers highly recommends this book.
+                        </p>
+                    </div>
                 </div>
+            </div>
+            <div class="row">
+                @foreach($book->reviewer as $reviewer)
+                    <div class="
+                        @if(count($book->reviewer) === 1) col-md-4 col-sm-6 col-md-offset-4
+                        @elseif(count($book->reviewer) === 2) col-md-6 col-sm-6
+                        @elseif(count($book->reviewer) === 3) col-md-4 col-sm-6
+                        @endif">
+                        <div class="media wow fadeInUp" data-wow-delay=".7s">
+                            <img
+                                class="media-object pull-left"
+                                src="{{ $reviewer->image }}"
+                                alt="{{ $reviewer->names }}"
+                                height="170"
+                            >
+                            <div class="media-body">
+                                <h4 class="media-heading">
+                                    {{ $reviewer->names }}
+                                </h4>
+                                <p>
+                                    {{ $reviewer->job_title }}
+                                </p>
+                                <p class="social-link">
+                                    @if($reviewer->facebook)
+                                        <a class="btn-facebook" href="{{ $reviewer->facebook }}">
+                                            <i class="fa fa-facebook"></i>
+                                        </a>
+                                    @endif
+
+                                    @if($reviewer->twitter)
+                                        <a class="btn-twitter" href="{{ $reviewer->twitter }}">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                    @endif
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+            <div class="row">
+                <br/>
+                @foreach($book->reviewer as $reviewer)
+                    <div class="
+                        @if(count($book->reviewer) === 1) col-md-4 col-sm-6 col-md-offset-4
+                        @elseif(count($book->reviewer) === 2) col-md-6 col-sm-6
+                        @elseif(count($book->reviewer) === 3) col-md-4 col-sm-6
+                        @endif">
+                        {{ $reviewer->message }}
+                    </div>
+                @endforeach
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-4 col-sm-4">
-                <div class="media wow fadeInUp" data-wow-delay=".7s">
-                    <img class="media-object pull-left" src="/images/team-1.png" alt="Image">
-                    <div class="media-body">
-                        <h4 class="media-heading">Matthew Graham</h4>
-                        <p>Co-Founder / CEO</p>
-                        <p class="social-link">
-                            <a class="btn-facebook" href=""><i class="fa fa-facebook"></i></a>
-                            <a class="btn-twitter" href=""><i class="fa fa-twitter"></i></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-                <div class="media wow fadeInUp" data-wow-delay="1.1s">
-                    <img class="media-object pull-left" src="/images/team-2.png" alt="Image">
-                    <div class="media-body">
-                        <h4 class="media-heading">Jonathon Gramble</h4>
-                        <p>Co-Founder / CEO</p>
-                        <p class="social-link">
-                            <a class="btn-facebook" href=""><i class="fa fa-facebook"></i></a>
-                            <a class="btn-twitter" href=""><i class="fa fa-twitter"></i></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-                <div class="media wow fadeInUp" data-wow-delay="1.4s">
-                    <img class="media-object pull-left" src="/images/team-3.png" alt="Image">
-                    <div class="media-body">
-                        <h4 class="media-heading">Jonathon Gramble</h4>
-                        <p>Co-Founder / CEO</p>
-                        <p class="social-link">
-                            <a class="btn-facebook" href=""><i class="fa fa-facebook"></i></a>
-                            <a class="btn-twitter" href=""><i class="fa fa-twitter"></i></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+    </section>
+@endif
