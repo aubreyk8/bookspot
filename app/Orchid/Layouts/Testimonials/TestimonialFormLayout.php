7cc755def6eba453efa3c35d37ac5b3110c33451
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-12 01:14:29 +0200

    Updated testimonial form

--- app/Orchid/Layouts/Testimonials/TestimonialFormLayout.php
+++ app/Orchid/Layouts/Testimonials/TestimonialFormLayout.php
@@ -2,6 +2,7 @@
 
 namespace App\Orchid\Layouts\Testimonials;
 
+use App\Services\BookLocator;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Layouts\Rows;
 use Orchid\Screen\Actions\Button;
@@ -15,23 +16,44 @@ class TestimonialFormLayout extends Rows
 {
 
     /**
+     * @var int|null
+     */
+    public int $book_id;
+
+    /**
+     * PublicationReviewFormLayout constructor.
+     * @param BookLocator $bookLocator
+     * @param array $layouts
+     */
+    public function __construct(BookLocator $bookLocator, array $layouts = [])
+    {
+        parent::__construct($layouts);
+        $this->book_id = $bookLocator->getBookId();
+    }
+
+    /**
      * @inheritDoc
      */
     protected function fields(): array
     {
         return [
-            Input::make('names')
+            Input::make('testimonial.id')
+                ->type('hidden'),
+            Input::make('testimonial.names')
                 ->title('Names')
                 ->placeholder('Names')
                 ->horizontal(),
-            Input::make('job_title')
+            Input::make('testimonial.job_title')
                 ->title('Job Title')
                 ->placeholder('Job Title')
                 ->horizontal(),
-            TextArea::make('message')
+            TextArea::make('testimonial.message')
                 ->title('Message')
                 ->placeholder('Message')
                 ->horizontal(),
+            Input::make('testimonial.book_id')
+                ->type('hidden')
+                ->value($this->book_id),
             Button::make('submit')
                 ->class('btn btn-primary')
                 ->icon('icon-save'),
