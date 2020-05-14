9bcb74e21a86b9c837ddfa4d5ebbd259e5da998c
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-14 14:37:23 +0200

    Added admin user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -18,32 +18,45 @@ class UserTableSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Aubrey',
             'last_name' => 'Hlungwane',
-            'email' => 'admin@admin.com',
+            'email' => 'Hlungwaneak@inspiredhost.co.za',
             'password' => bcrypt('admin'),
             'permissions' => $this->getAdminPermissions(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
 
-        DB::table('users')->insert([
-            'first_name' => 'Mike',
-            'last_name' => 'Hove',
-            'email' => 'mike@admin.com',
-            'password' => bcrypt('admin'),
-            'permissions' => $this->getPublisherPermissions(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        if (app()->environment('local')) {
 
-        DB::table('users')->insert([
-            'first_name' => 'Prince',
-            'last_name' => 'Sono',
-            'email' => 'prince@admin.com',
-            'password' => bcrypt('admin'),
-            'permissions' => $this->getPublisherPermissions(),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+            DB::table('users')->insert([
+                'first_name' => 'Aubrey',
+                'last_name' => 'Hlungwane',
+                'email' => 'admin@admin.com',
+                'password' => bcrypt('admin'),
+                'permissions' => $this->getAdminPermissions(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+
+            DB::table('users')->insert([
+                'first_name' => 'Mike',
+                'last_name' => 'Hove',
+                'email' => 'mike@admin.com',
+                'password' => bcrypt('admin'),
+                'permissions' => $this->getPublisherPermissions(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+
+            DB::table('users')->insert([
+                'first_name' => 'Prince',
+                'last_name' => 'Sono',
+                'email' => 'prince@admin.com',
+                'password' => bcrypt('admin'),
+                'permissions' => $this->getPublisherPermissions(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 
     /**
