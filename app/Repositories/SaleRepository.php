5629f980cc87d052b2b5c867d629296811903656
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-14 13:56:29 +0200

    Added download counter

--- app/Repositories/SaleRepository.php
+++ app/Repositories/SaleRepository.php
@@ -20,6 +20,9 @@ class SaleRepository extends BaseRepository
     public function getDownload(string $link): Attachment
     {
         $sale = Sale::where('link', $link)->get()->first();
+        $sale->downloads = isset($sale->downloads) ? $sale->downloads ++ : 1;
+        $sale->save();
+
         return $sale->book->attachment()->get()->last();
     }
 }
