8586d9eabb034351fdfa5161baf11f16b796b011
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-15 21:55:56 +0200

    Added sale repository

--- app/Repositories/SaleRepository.php
+++ app/Repositories/SaleRepository.php
@@ -3,7 +3,10 @@
 namespace App\Repositories;
 
 use App\Models\Sale;
+use App\Constants\DashboardView;
+use Illuminate\Support\Facades\Auth;
 use Orchid\Attachment\Models\Attachment;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 /**
  * Class SaleRepository
@@ -11,7 +14,24 @@ use Orchid\Attachment\Models\Attachment;
  */
 class SaleRepository extends BaseRepository
 {
+    /**
+     * @var string
+     */
     protected string $prototype = Sale::class;
+
+    /**
+     * @param string|null $needle
+     * @param string $column
+     * @return LengthAwarePaginator
+     */
+    public function list(string $needle = null, string $column = 'id'): LengthAwarePaginator
+    {
+        if (Auth::user()->inRole('administrator')) {
+            return Sale::paginate(DashboardView::PAGINATION_LENGTH);
+        }
+
+        return Sale::where('user_id', Auth::user()->id)->paginate(DashboardView::PAGINATION_LENGTH);
+    }
 
     /**
      * @param string $link
