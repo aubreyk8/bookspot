9af5a3e1d5d812666daf6c07d677ff6c98d4e14b
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-16 15:59:36 +0200

    Update migration

--- database/migrations/2020_05_05_204131_create_themes_table.php
+++ database/migrations/2020_05_05_204131_create_themes_table.php
@@ -16,11 +16,13 @@ class CreateThemesTable extends Migration
         Schema::create('themes', function (Blueprint $table) {
             $table->id();
             $table->string('theme')->default('fisher');
-            $table->string('primary_color');
-            $table->string('secondary_color');
-            $table->string('icon_color');
-            $table->json('cover_image_height');
-            $table->json('secondary_image_height');
+            $table->string('primary_color')->default('0000cc');
+            $table->string('secondary_color')->default('FED600');
+            $table->string('icon_color')->default('CD483E');
+            $table->string('cover_image_height')->default(300);
+            $table->string('cover_image_width')->default(300);
+            $table->string('secondary_image_height')->default(300);
+            $table->string('secondary_image_width')->default(300);
             $table->string('font_color')->default('FFFFFF');
             $table->bigInteger('book_id', false, true);
             $table->timestamps();
