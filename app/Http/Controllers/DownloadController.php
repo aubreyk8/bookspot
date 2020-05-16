ecee0eddb65baf6fbd9ea1e1842271abe80c0b6a
Aubrey Hlungwane <Hlungwaneak@protonmail.com>
2020-05-16 16:41:01 +0200

    Fix file downloads

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\SaleRepository;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
 
 /**
  * Class DownloadController
@@ -15,13 +17,26 @@ class DownloadController extends Controller
      * @param $id
      * @param SaleRepository $saleRepository
      * @return BinaryFileResponse
+     * @throws FileNotFoundException
      */
     public function download(string $id, SaleRepository $saleRepository): BinaryFileResponse
     {
         $attachment = $saleRepository->getDownload($id);
 
+        $fileContents = Storage::disk($attachment->disk)
+            ->get(
+                $attachment->path .
+                $attachment->name .
+                '.' .
+                $attachment->extension
+            );
+
+        $tempFile = storage_path('app/tmp/' . $attachment->name);
+
+        file_put_contents($tempFile, $fileContents);
+
         return response()->download(
-            public_path('storage/' . $attachment->path . $attachment->name . '.' . $attachment->extension),
+            $tempFile,
             $attachment->original_name,
             [
                 'Content-Type' => $attachment->getMimeType(),
